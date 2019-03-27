509f78447136dd9a77521f623ab9d7b7b5dee899
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-03-27 14:39:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,16 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    if params[:artist_id]
-      artist = Artist.find_by(id: params[:artist_id])
-      if artist.nil?
-        redirect_to artists_path, alert: "Artist not found."
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found."
       else
         @song = Song.new(artist_id: params[:artist_id])
       end
-    else
-      @song = Song.new(artist_id: params[:artist_id])
-    end
 end
 
   def create
