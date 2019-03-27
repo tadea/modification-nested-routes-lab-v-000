04c0e1363582ea2940dbb3e213f5698e23489f74
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-03-27 14:30:39 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_select(song,artist)
+  def artist_select(artists, song)
     if artist
       artist.name
     else
