fea6607305ebfad3b888204aabd77896b4b2cf5d
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-03-27 14:03:35 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,11 +3,11 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_select(song, artist)
-    if song.artist.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    else
+  def artist_select(artist, song)
+    if artist
       artist.name
+    else
+      select_tag "song[:artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
   end
 end
